046739307be7f3f25735bf440dc7dccf9f41b2bc
Mohamud E Hasen <mohmudes4@gmail.com>
2023-05-16 18:01:49 -0500

    added more js fucntionality

--- script.js
+++ script.js
@@ -1,21 +1,52 @@
 const container = document.querySelector('.container');
-const seat = document.querySelectorAll('.row .seat:not(.occupied)');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+populateUI();
+
 let ticketPrice = +movieSelect.value;
+
+//save selected movie, index and price
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+} 
 //update selected count and seats
 function updateSelectedCount(){
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+        const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat))
+
+        localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
     const selectedSeatsCount = selectedSeats.length;
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+//Get data from localStorage and populate UI 
+function populateUI(){
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    if(selectedSeats !== null && selectedSeats.length > 0){
+        seats.forEach((seat, index) => {
+            if(selectedSeats.indexOf(index) > -1){
+                seat.classList.add('selected');
+            }
+        })
+    }
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+    if(selectedMovieIndex !== null){
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+}
+
 //moie select event
 movieSelect.addEventListener('change', e =>{
     ticketPrice = +e.target.value;
+
+    setMovieData(e.targer.selectedIndex, e.target.value);
     updateSelectedCount();
 })
 
